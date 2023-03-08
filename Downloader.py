14c95a32b5bd848cbb0ee40ef840c8343f7b51b9
dgg1dbg <jeongmin.lihi@gmail.com>
2023-03-08 13:11:08 +0000

    added technical indicators

--- Downloader.py
+++ Downloader.py
@@ -1,11 +1,12 @@
 import pandas as pd
-
+from stockstats import wrap
 class Downloader:
-    def __init__(self, binance, symbol, timeframe, since):
+    def __init__(self, binance, symbol, timeframe, since, tech_indicator):
         self.binance = binance
         self.symbol = symbol
         self.timeframe = timeframe
         self.since = binance.parse8601(since)
+        self.tech_indicator = tech_indicator
     
     def download(self):
         y = []
@@ -18,5 +19,8 @@ class Downloader:
         df = pd.DataFrame(y, columns=['datetime', 'open', 'high', 'low', 'close', 'volume'])
         df['datetime'] = pd.to_datetime(df['datetime'], unit='ms')
         df.set_index('datetime', inplace=True)
+        tmp = wrap(df)
+        tmp = tmp[self.tech_indicator]
+        tmp = tmp.drop(['high', 'low'], axis=1)
         return df
 
